e2e8bc4f54a44d106cdf3c8345c8a07dd115a92a
Krystian Postek <krystian@postek.eu>
2021-12-03 15:30:04 +0100

    Commit PoC code

---
+++ STATUT.md
@@ -0,0 +1,7 @@
+# Statut serwera “PJATK Informatyka 2021/25”
+
+## 1
+
+ 1. aaaa 
+ 2. Bruh
+ 3. section
---
+++ push.js
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import showdown from "showdown"
+import { JSDOM } from "jsdom"
+import { readFile } from "fs/promises"
+import { Webhook, MessageBuilder } from "discord-webhook-node"
+
+
+// const testStr = "# whatever"
+const doc = await readFile("STATUT.md")
+
+const dom = JSDOM.fragment(new showdown.Converter().makeHtml(doc.toString()))
+
+for (const child of dom.children) {
+    const hook = new Webhook(process.env.WEBHOOK)
+    const embed = new MessageBuilder()
+
+    console.log(child.tagName, child.innerHTML)
+    switch (child.tagName.toLowerCase()) {
+        case 'h1': embed.setTitle(child.innerHTML); break
+        case 'h2': embed.setTitle('§ ' + child.innerHTML); break
+        case 'ol': let i = 0; child.querySelectorAll('li').forEach(li => {
+            embed.addField(++i + '.', li.innerHTML, false)
+        }); break
+    }
+
+    await hook.send(embed)   
+}
