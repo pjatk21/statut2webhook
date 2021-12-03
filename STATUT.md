ea3a1581a45d40316004b7a5d93fd806980dbafb
Krystian Postek <krystian@postek.eu>
2021-12-03 17:03:54 +0100

    Make some improvements

--- STATUT.md
+++ STATUT.md
@@ -1,7 +1,12 @@
 # Statut serwera “PJATK Informatyka 2021/25”
 
-## 1
-
+## Whatever
+bla bla bla
  1. aaaa 
  2. Bruh
  3. section
+
+## ES6 FTW
+java sucks like java
+ 1. tomaszew
+ 2. dzawamajster
--- push.js
+++ push.js
@@ -4,29 +4,64 @@ import { JSDOM } from "jsdom"
 import { readFile } from "fs/promises"
 import { Webhook, MessageBuilder } from "discord-webhook-node"
 
+// load file
 const doc = await readFile("STATUT.md")
 
+// change md to html which change to a DOM
 const dom = JSDOM.fragment(new showdown.Converter().makeHtml(doc.toString()))
 
+// create blocks
+let titleBlock = undefined
+const blocks = new Array()
+let helper = {
+  title: undefined,
+  description: undefined,
+  points: [],
+}
+
 for (const child of dom.children) {
-  const hook = new Webhook(process.env.WEBHOOK)
+  if (child.tagName === 'H1') {
+    titleBlock = child
+  } else if (child.tagName === 'H2') {
+    if (helper.points.length > 0) { 
+      blocks.push(helper)
+      helper = {
+        title: child,
+        description: undefined,
+        points: [],
+      }
+    } else {
+      helper.title = child
+    }
+  } else if (child.tagName === 'OL') {
+    for (const row of child.getElementsByTagName('li')) {
+      helper.points.push(row)
+    }
+  } else if (child.tagName === 'P') helper.description = child
+}
+blocks.push(helper) // add last block
+
+// transform raw blocks into embeds
+const embeds = new Array()
+embeds.push(
+  new MessageBuilder()
+    .setTitle(titleBlock.innerHTML)
+)
+
+for (const block of blocks) {
   const embed = new MessageBuilder()
+  embed.setTitle(block.title.innerHTML)
+  embed.setDescription(block.description.innerHTML)
 
-  console.log(child.tagName, child.innerHTML)
-  switch (child.tagName.toLowerCase()) {
-    case "h1":
-      embed.setTitle(child.innerHTML)
-      break
-    case "h2":
-      embed.setTitle("§ " + child.innerHTML)
-      break
-    case "ol":
-      let i = 0
-      child.querySelectorAll("li").forEach((li) => {
-        embed.addField(++i + ".", li.innerHTML, false)
-      })
-      break
+  let counter = 0
+  for (const row of block.points) {
+    embed.addField(`${++counter}.`, row.innerHTML)
   }
+  embeds.push(embed)
+}
 
+const hook = new Webhook(process.env.WEBHOOK)
+for (const embed of embeds) {
+  console.log('Pushing embed', embed)
   await hook.send(embed)
 }
